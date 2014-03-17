ae1706e4e3ffbc28d00b6aea43345ad29876cc1e
Brian Lee <bcl54@cornell.edu>
2014-03-17 16:26:11 -0400

    half a method

--- WSD.java
+++ WSD.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -6,6 +11,7 @@ import java.nio.file.Paths;
 public class WSD {
 
 	public String dictionaryText;
+	public String trainingText;
 	
 	public static void parseDictionary() {
 		String content = new String(Files.readAllBytes(Paths.get(trainFile.getAbsolutePath())));
@@ -24,6 +30,32 @@ public class WSD {
 	public static String[] lookUpDictionary(String target) {
 		
 
+	}
+	
+	//Return the sense/definition of the target word
+	public int parseTestData(String filename) {
+		
+		File file = new File(filename);
+		BufferedReader bufferedReader = null;
+		
+		try {
+			bufferedReader = new BufferedReader(new FileReader(file));
+			String line = bufferedReader.readLine();
+			
+			while (line != null) {
+				
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			bufferedReader.close();
+		}
+		
+		
+		
+		return 0;
 	}
 	
 }
