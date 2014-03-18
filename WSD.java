1af45850be8921d3ad3a7a9077cc40dc403b4611
Brian Lee <bcl54@cornell.edu>
2014-03-18 18:32:07 -0400

    made some changes

--- WSD.java
+++ WSD.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 
 
@@ -15,7 +16,11 @@ public class WSD {
 
 	public static String dictionaryText;
 	public static HashMap<String, HashMap<Integer, String[]>> dictMap = new HashMap<>();
-	public String trainingText;
+	public static String trainingText;
+	//string of word points to an index/position in the text block
+	public static ArrayList<HashMap<String,String>> targetPointers;
+	//array to which hashmap of pointers points to
+	public static ArrayList<String[]> targetSensesDefinitions;
 	
 	public static final String dict_path = "src/Data/dictionary.xml";
 	
@@ -55,9 +60,49 @@ public class WSD {
 		return defs.toArray(new String[defs.size()]);
 	}
 	
-	//Return the sense/definition of the target word
-	public int parseTestData(String filename) {
+	/*
+	 * Make a new array with # of entries = # of senses of the target word
+	 * populate hashmap pointing to the index at which it is located
+	 */
+	public static void processTarget(String target) {
+		HashMap<Integer,String[]> targetMap = lookUpDictionaryTarget(target);
+		int numSenses = targetMap.size();
 		
+		targetPointers = new ArrayList<HashMap<String,String>>(numSenses);
+		targetSensesDefinitions = new ArrayList<String[]>(numSenses);
+		
+		//populate targetSensesDefinitions
+		for (int i = 0; i < numSenses; i++) {
+			//retrieve ith sense and get string of words from defintion
+			String[] senseStrings = targetMap.get(i);
+			
+			//create targetPointers in senseStrings, pointers are delimited by commas
+			HashMap<String,String> targ = new HashMap<String,String>();
+			for (int j = 0; j < senseStrings.length; j++) {
+				String s = senseStrings[j].toLowerCase();
+				//if string does not exist, add new entry into hashmap
+				if (!targ.containsKey(s)) {
+					String index = String.valueOf(j);
+					targ.put(s,index);
+				} else {
+					//else if not empty, add to string
+					String index = targ.get(s);
+					index = index + "," + String.valueOf(j);
+					targ.put(s,index);
+				}
+			}
+			
+			targetPointers.add(targ);
+			//copy senseStrings to targetSensesDefinitions array
+			targetSensesDefinitions.add(senseStrings);
+		}
+		
+	}
+	
+	/*
+	 * Return the sense/definition of the target word for each line
+	 */
+	public static int[] parseTestData(String filename) {
 		File file = new File(filename);
 		BufferedReader bufferedReader = null;
 		
@@ -66,6 +111,80 @@ public class WSD {
 			String line = bufferedReader.readLine();
 			
 			while (line != null) {
+				//store target word
+				int targetIndex = line.indexOf("|");
+				String target = line.substring(0,targetIndex).trim();
+				//update new string without target
+				line = line.substring(targetIndex+1,line.length());
+				
+				//retrieve block of text
+				int textIndex = line.indexOf("|");
+				String text = line.substring(textIndex,line.length()).trim();
+				
+				//retrieve hashmap for different senses of target word
+				HashMap<Integer,String[]> targetWord = lookUpDictionaryTarget(target);
+				int numSenses = targetWord.size();
+				processTarget(target);
+				
+				//split text and remove ,.$'`%
+				String[] splitText = text.split(" ");
+				HashSet<String> notImportant = new HashSet<String>();
+				notImportant.add(",");
+				notImportant.add(".");
+				notImportant.add("$");
+				notImportant.add("'");
+				notImportant.add("`");
+				notImportant.add("%%");
+				
+				//TODO: Lemmatize target word and other words in block of text
+				int posIndex = target.indexOf(".");
+				String targetRoot = target.substring(0,posIndex);
+				
+				//HashMap<String,String[]> contextWords = new HashMap<String,String[]>();
+				
+				//points for each sense
+				int[] points = new int[numSenses];
+				
+				for (String s: splitText) {
+					//remove unimportant characters and target word
+					if (!(notImportant.contains(s) || s.equals(targetRoot))) {
+						String[] contextMeaning = lookUpDictionaryContext(s);
+						
+						//for each word, add the points accordingly for each sense
+						//+1 for each word occurrence, +2 if there are consecutive words
+						for (String t: contextMeaning) {
+							for (int i = 0; i < numSenses; i++) {
+								//if the key exists, add one for each key and check surrounding
+								//keys to see if there are consecutive matches
+								if (targetPointers.get(i).containsKey(t)) {
+									points[i]++;
+									
+									//get indices (delimited by commas), convert to ints
+									String[] tempIndices = targetPointers.get(i).get(t).split(",");
+									int[] indices = new int[tempIndices.length];
+									for (int k = 0; k < tempIndices.length; k++)
+										indices[k] = Integer.parseInt(tempIndices[k]);
+									
+									for (int k = 0; k < indices.length; k++) {
+										//store previous and next word surrounding context word
+										String prev = 
+									}
+									
+									
+								}
+							}
+						}
+					}
+				}
+				
+				line = bufferedReader.readLine();
+				//TODO: Check if it is actually in the dictionary
+				
+				
+				//TODO: Change numbers into 1 entry?
+				//TODO: Deal with contractions b/c we removed apostrophes
+				
+				
 				
 			}
 		} catch (FileNotFoundException e) {
@@ -78,7 +197,7 @@ public class WSD {
 		
 		
 		
-		return 0;
+		return new int[0];
 	}
 	
 }
