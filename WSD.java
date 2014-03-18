f828e791ac85b6fe2bb33b51368119af45c3bc98
mzw4 <mzw4@cornell.edu>
2014-03-18 18:53:24 -0400

    Added wordnet lookup, can retreive definitions, adding filtering function to lemmatize, etc

--- WSD.java
+++ WSD.java
@@ -9,6 +9,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
+import edu.smu.tspell.wordnet.Synset;
+import edu.smu.tspell.wordnet.SynsetType;
+import edu.smu.tspell.wordnet.WordNetDatabase;
+
 
 
 public class WSD {
@@ -16,16 +20,14 @@ public class WSD {
 	public static String dictionaryText;
 	public static HashMap<String, HashMap<Integer, String[]>> dictMap = new HashMap<>();
 	public String trainingText;
+		
+	private static WordNetDatabase database;
 	
-	public static final String dict_path = "src/Data/dictionary.xml";
+	private static String[] stopWords = {"the"
 	
-	public static void parseDictionary(File dictFile) {
-		String dictionaryText;
-		try {
-			dictionaryText = new String(Files.readAllBytes(Paths.get(dictFile.getAbsolutePath())));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public static void initDictionary() {
+		System.setProperty("wordnet.database.dir", "/Users/mike/Documents/workspace-NLP/WordNet-3.0/dict/");
+		database = WordNetDatabase.getFileInstance();
 	}
 	
 	/*
@@ -37,22 +39,85 @@ public class WSD {
 	}
 	
 	/*
-	 * Look up target word in dictionary.
-	 * Returns a map in the form <sense id, array of words in that sense> 
+	 * Look up target word in dictionary. Need to keep track of senses.
+	 * Returns a map in the form <sense id, array of words in that sense definition> 
 	 */
 	public static HashMap<Integer, String[]> lookUpDictionaryTarget(String target) {
-		return dictMap.get(target);
+		Synset[] synset = queryWordNet(target);
+		if(synset == null) {
+			return null;
+		}
+		
+		HashMap<Integer, String[]> defs = new HashMap<>();
+		for(int i = 0; i < synset.length; i++) {
+			// Process and tokenize
+			String processed = synset[i].getDefinition().replaceAll("([(),!.?;:])", " $1 ");
+			String[] tokens = processed.split("\\s+");
+			
+			// Filter irrelevant words and insert into map
+			defs.put(i, filterFeatures(tokens));
+		}
+		return defs;
 	}
 	
 	/*
 	 * Look up context feature words. Senses don't matter.
+	 * Returns a list of tokens aggregated from all definitions of the target
 	 */
 	public static String[] lookUpDictionaryContext(String target) {
-		ArrayList<String> defs = new ArrayList<>();
-		for(String[] tokens: dictMap.get(target).values()) {
-			defs.addAll(Arrays.asList(tokens));
+		Synset[] synset = queryWordNet(target);
+		if(synset == null) {
+			return null;
 		}
-		return defs.toArray(new String[defs.size()]);
+		
+		ArrayList<String> tokens = new ArrayList<>();
+		for(Synset form: synset) {
+			// Process and tokenize
+			String processed = form.getDefinition().replaceAll("([(),!.?;:])", " $1 ");
+			String[] strings = processed.split("\\s+");
+			
+			// Filter irrelevant features and insert into list
+			tokens.addAll(Arrays.asList(filterFeatures(strings)));
+		}
+		return tokens.toArray(new String[tokens.size()]);
+	}
+	
+	/*
+	 * Look up target word in WordNet.
+	 * Returns an Synset array 
+	 */
+	public static Synset[] queryWordNet(String target) {
+		if(target.length() < 2) {
+			System.out.println("Invalid target word \"" + target + "\"");
+			return null;
+		}
+		String type = target.substring(target.length() - 1);
+		
+		Synset[] synset = null;
+		switch(type) {
+		case "n":
+			synset = database.getSynsets(target.substring(0, target.indexOf(".")), SynsetType.NOUN);
+			break;
+		case "v":
+			synset = database.getSynsets(target, SynsetType.VERB);
+			break;
+		case "a":
+			synset = database.getSynsets(target, SynsetType.ADJECTIVE);
+			break;
+		default:
+			break;
+		}
+		return synset;
+	}
+	
+	/*
+	 * Filter features by lemmatizing, then removing irrelevant features from the list,
+	 * and keeping only lexemes that may have an impact on semantic meaning
+	 */
+	private static String[] filterFeatures(String[] features) {
+		ArrayList<String> filtered = new ArrayList<>();
+		
+		return features;
 	}
 	
 	//Return the sense/definition of the target word
@@ -73,12 +138,16 @@ public class WSD {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			bufferedReader.close();
+			//bufferedReader.close();
 		}
-		
-		
-		
+
 		return 0;
+	}
+	
+	public static void main(String[] args) {
+		WSD.initDictionary();
+		WSD.lookUpDictionaryContext("chair.n");
+		
 	}
 	
 }
